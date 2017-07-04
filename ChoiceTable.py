34666d506f3e1b312b60ac5252ee92527e338f2b
Laurin Erlacher <laurin.erlacher@gmail.com>
2017-07-04 17:39:10 +0200

    added factory methods for creating modifiers removed duplicate mods from modifier.yaml: ==> +1, +2, proficient mods for all races ==> proficiency mod for all classes

--- ChoiceTable.py
+++ ChoiceTable.py
@@ -21,10 +21,10 @@ class StateTable:
                 items = self.widget.checkedItems()
             activate = items - self.activeMod
             print("activate",activate)
-            getModifier("Human_plus").connect(getValues(activate))
+            getModifier("Human_plus1").connect(getValues(activate))
             deactivate = self.activeMod - items
             print("deactivate",deactivate)
-            getModifier("Human_plus").disconnect(getValues(deactivate))
+            getModifier("Human_plus1").disconnect(getValues(deactivate))
             self.activeMod = items
 
         def destroy(self):
@@ -77,7 +77,7 @@ class StateTable:
             self.extra_ui.setupUi(self.tab_human)
             self.tab = tabRoot.addTab(self.tab_human, "Human")
             self.extra_ui.checkBox_variantTraits.stateChanged.connect(self.altHumanTrait)
-            getModifier("Human_plus").connect(getValues(("strength", "dexterity", "constitution", "intelligence","wisdom","charisma")))
+            getModifier("Human_plus1").connect(getValues(("strength", "dexterity", "constitution", "intelligence","wisdom","charisma")))
 
         def exitHuman(self):
             self.extra_ui.checkBox_variantTraits.setCheckState(False)
@@ -86,23 +86,23 @@ class StateTable:
             del self.tab_human
             del self.extra_ui
             del self.tab
-            getModifier("Human_plus").disconnect(getValues(("strength", "dexterity", "constitution", "intelligence","wisdom","charisma")),True)
-            getModifier("Human_plus").disconnect(getValue("prof_skills_learnChildren"),True)
+            getModifier("Human_plus1").disconnect(getValues(("strength", "dexterity", "constitution", "intelligence","wisdom","charisma")),True)
+            getModifier("Human_plus1").disconnect(getValue("prof_skills_learnChildren"),True)
             getValue("listWidget_proficiencies_VisualUpdate").changeSignal.emit()
 
         def altHumanTrait(self):
             b = self.extra_ui.checkBox_variantTraits.checkState()
             if b:
-                getModifier("Human_plus").disconnect(getValues(("strength", "dexterity", "constitution", "intelligence","wisdom","charisma")),True)
-                getModifier("Human_plus").connect(getValue("prof_skills_learnChildren"))
+                getModifier("Human_plus1").disconnect(getValues(("strength", "dexterity", "constitution", "intelligence","wisdom","charisma")),True)
+                getModifier("Human_plus1").connect(getValue("prof_skills_learnChildren"))
                 #self.skill = StateTable.Human_SkillChoice(self.extra_ui.comboBox_skill)
                 self.feat = StateTable.FeatChoice(self.extra_ui.comboBox_feat)
                 self.score = StateTable.Choice2AbilityScore(self.extra_ui.comboBox_abilityScore)
                 for i in range(self.extra_ui.comboBox_abilityScore.count()):
                     self.extra_ui.comboBox_abilityScore.setItemChecked(i, False)
             else:
-                getModifier("Human_plus").connect(getValues(("strength", "dexterity", "constitution", "intelligence","wisdom","charisma")))
-                getModifier("Human_plus").disconnect(getValue("prof_skills_learnChildren"),True)
+                getModifier("Human_plus1").connect(getValues(("strength", "dexterity", "constitution", "intelligence","wisdom","charisma")))
+                getModifier("Human_plus1").disconnect(getValue("prof_skills_learnChildren"),True)
                 getValue("listWidget_proficiencies_VisualUpdate").changeSignal.emit()
                 self.score.destroy()
                 #self.skill.destroy()
--- ModifierTable.py
+++ ModifierTable.py
@@ -23,6 +23,68 @@ class ModifierTable:
                     order = meta[key]
                 if key=="text":
                     text = meta[key]
-            self._data[name] = ValueModifier(l, text, order)
+            self.newModifier(name, ValueModifier(l, text, order))
+
+        for auto in (AutoModRace,AutoModClass):
+            c = auto()
+            self.autoModInit(c)
+
+    def newModifier(self,name,modifier):
+        assert modifier not in self._data.values()
+        assert name not in self._data.keys()
+        self._data[name] = modifier
+
     def __getattr__(self,name):
         return self._data[name]
+
+    def autoModInit(self,r):
+        for name, mod in r.getMods():
+            self.newModifier(name,mod)
+
+
+class AutoModBase:
+    lambdaBase = "x"
+    textBase = "AutoModBase"
+    nameBase = "AutoModName"
+    def __init__(self):
+        self.values=()
+    def createFromList(self,values):
+        for value in values:
+            for v in self.new(value):
+                yield v
+    def newFromFormat(self,lambda_args=(),text_args=(),name_args=(),order=0):
+        l = self.lambdaBase.format(*lambda_args)
+        l=eval("lambda x:"+l)
+        text = self.textBase.format(*text_args)
+        name = self.nameBase.format(*name_args)
+        order=int(order)
+        return name, ValueModifier(l,text,order)
+    def new(self,value):
+        yield self.newFromFormat(value,value)
+    def getMods(self):
+        for mod in self.createFromList(self.values):
+            yield mod
+
+class AutoModRace(AutoModBase):
+    lambdaBase = "x{0}"
+    textBase = "{0}"
+    nameBase = "{0}_{1}"
+    def __init__(self):
+        f = open(getDirectoryPrefix()+"data/character/automod_races.yaml")
+        self.values = yaml.load(f)
+        f.close()
+    def new(self,value):
+        yield self.newFromFormat(("+1",), (value+": +1"), (value, "plus1"))
+        yield self.newFromFormat(("+2",), (value+": +2"), (value, "plus2"))
+        yield self.newFromFormat(("+1",), (value), (value, "proficient"))
+
+class AutoModClass(AutoModBase):
+    lambdaBase = "x{0}"
+    textBase = "{0}"
+    nameBase = "{0}_proficient"
+    def __init__(self):
+        f = open(getDirectoryPrefix()+"data/character/automod_classes.yaml")
+        self.values = yaml.load(f)
+        f.close()
+    def new(self,value):
+        yield self.newFromFormat(("+1",), (value,), (value,))
